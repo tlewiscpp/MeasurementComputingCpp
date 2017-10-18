149302bb0a8219d558d97b0c042064215385ccd0
Tyler Lewis <tlewiscpp@gmail.com>
2017-10-18 19:48:25 -0400

    Auto-Commit - Z170A-Titanium-PC

--- init-repository.sh
+++ init-repository.sh
@@ -1,13 +1,20 @@
 #!/bin/bash -e
 
-cd hidapi-root || { echo "Could not enter hidapi-root, exiting"; exit 1; }
-./clone-source.sh
-cd ../
+#cd hidapi-root || { echo "Could not enter hidapi-root, exiting"; exit 1; }
+#./clone-source.sh
+#cd ../
 
-cd libusb-root || { echo "Could not enter libusb-root, exiting"; exit 1; }
-./clone-source.sh
-cd ../
+#cd libusb-root || { echo "Could not enter libusb-root, exiting"; exit 1; }
+#./clone-source.sh
+#cd ../
 
-cd MeasurementComputingCore/mccusb-root || { echo "Could not enter mccusb-root, exiting"; exit 1; }
-./clone-source.sh
-cd ../
+#cd MeasurementComputingCore/mccusb-root || { echo "Could not enter mccusb-root, exiting"; exit 1; }
+#./clone-source.sh
+#cd ../
+
+if [[ ! -f ".repo-init" ]]; then
+    git submodule update --init --recursive
+    touch .repo-init
+else
+    git submodule update --recursive --remote
+fi
