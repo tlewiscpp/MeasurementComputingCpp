4cb427de1bb5a77e11a0e4bfeb019cbe5af0a2f0
Tyler Lewis <tlewiscpp@gmail.com>
2018-02-13 07:59:16 -0500

    Auto-Commit - Inspiron-5559-PC

--- init-repository.sh
+++ init-repository.sh
@@ -1,17 +1,5 @@
 #!/bin/bash -e
 
-#cd hidapi-root || { echo "Could not enter hidapi-root, exiting"; exit 1; }
-#./clone-source.sh
-#cd ../
-
-#cd libusb-root || { echo "Could not enter libusb-root, exiting"; exit 1; }
-#./clone-source.sh
-#cd ../
-
-#cd MeasurementComputingCore/mccusb-root || { echo "Could not enter mccusb-root, exiting"; exit 1; }
-#./clone-source.sh
-#cd ../
-
 if [[ ! -f ".repo-init" ]]; then
     git submodule update --init --recursive
     touch .repo-init
@@ -19,4 +7,3 @@ else
     git submodule update --recursive --remote
 fi
 
-cp -R libusb-root/libusb/libusb/libusb.h mccusb-root/libusb-1.0/
